6d4e314fd2dafc2f2d03919249a16a5f8ea6cee0
Mohammed Hesham <mohammed.hesham@iskraemeco.com>
2022-04-14 08:48:28 +0200

    Initial Commit

---
+++ Untitled-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct padding_s1
+{
+    uint8_t  foo1;
+    uint16_t foo2;
+    uint32_t foo3;
+    uint8_t  foo4;
+} padding1_t;
+
+typedef struct padding_s2
+{
+    uint32_t foo3;
+    uint16_t foo2;
+    uint8_t  foo1;
+    uint8_t  foo4;
+} padding2_t;
+
+int main(void)
+{
+    printf("%ld\n", sizeof(padding2_t));
+    return 0;
+}
